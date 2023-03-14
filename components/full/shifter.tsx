70f9ecac804a541500dcb945eade372e0c1f0bc0
Eldest Pasirula <aknutman@gmail.com>
2023-03-14 22:17:27 +0700

    shifter body layout

--- components/full/shifter.tsx
+++ components/full/shifter.tsx
@@ -7,11 +7,11 @@ export default function Shifter(ShifterData: ShifterModel) {
   console.log("ShifterData: ", ShifterData);
 
   return (
-    <div className="flex flex-row h-full">
+    <div className="flex h-full flex-row">
       <div className="basis-1/6">
         <Sidebar />
       </div>
-      <div className="basis-5/6 bg-gray-100">
+      <div className="basis-5/6 bg-gray-200">
         <BodyShifter />
       </div>
     </div>
--- components/partial/body.shifter.tsx
+++ components/partial/body.shifter.tsx
@@ -1,3 +1,135 @@
+import {
+  BriefcaseIcon,
+  CalendarIcon,
+  ArrowPathIcon,
+  CurrencyDollarIcon,
+  MapPinIcon,
+  PlusIcon,
+  ChevronRightIcon,
+  ChevronLeftIcon,
+} from "@heroicons/react/20/solid";
+import React from "react";
+
 export default function BodyShifter() {
-  return <>Body Shifter is worked!</>;
+  return (
+    <>
+      <div className="bg-white p-3 shadow lg:flex lg:items-center lg:justify-between">
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+            Title is Here
+          </h2>
+          <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+            <div className="mt-2 flex items-center text-sm text-gray-500">
+              <BriefcaseIcon
+                className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
+              />
+              Full-time
+            </div>
+            <div className="mt-2 flex items-center text-sm text-gray-500">
+              <MapPinIcon
+                className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
+              />
+              Remote
+            </div>
+            <div className="mt-2 flex items-center text-sm text-gray-500">
+              <CurrencyDollarIcon
+                className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
+              />
+              $120k &ndash; $140k
+            </div>
+            <div className="mt-2 flex items-center text-sm text-gray-500">
+              <CalendarIcon
+                className="mr-1.5 h-5 w-5 flex-shrink-0 text-gray-400"
+                aria-hidden="true"
+              />
+              Closing on January 9, 2020
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5 flex lg:mt-0 lg:ml-4">
+          <span className="hidden sm:block">
+            <button
+              type="button"
+              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+            >
+              <ArrowPathIcon
+                className="-ml-0.5 mr-1.5 h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+              Refresh
+            </button>
+          </span>
+
+          <span className="sm:ml-3">
+            <button
+              type="button"
+              className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+              Add New
+            </button>
+          </span>
+        </div>
+      </div>
+
+      <div className="m-5 shadow">
+        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+          Table will be here
+        </div>
+        <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+          <div className="sm:flex sm:flex-1 sm:items-center sm:justify-between">
+            <div>
+              <p className="text-sm text-gray-700">
+                Showing <span className="font-medium">1</span> to{" "}
+                <span className="font-medium">10</span> of{" "}
+                <span className="font-medium">97</span> results
+              </p>
+            </div>
+            <div>
+              <nav
+                className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                aria-label="Pagination"
+              >
+                <a
+                  href="#"
+                  className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                >
+                  <span className="sr-only">Previous</span>
+                  <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+                </a>
+                {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+                <a
+                  href="#"
+                  aria-current="page"
+                  className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  1
+                </a>
+                <a
+                  href="#"
+                  className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                >
+                  2
+                </a>
+                <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+                  ...
+                </span>
+                <a
+                  href="#"
+                  className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                >
+                  <span className="sr-only">Next</span>
+                  <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
