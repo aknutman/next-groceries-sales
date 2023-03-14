2dc329c1fe5f61649a36a6cb1e9ae70e95bc4771
Eldest Pasirula <aknutman@gmail.com>
2023-03-14 22:33:54 +0700

    table sample

--- components/partial/body.shifter.tsx
+++ components/partial/body.shifter.tsx
@@ -78,8 +78,36 @@ export default function BodyShifter() {
 
       <div className="m-5 shadow">
         <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-          Table will be here
+          <table className="w-full">
+            <thead>
+              <tr className="border-b-2">
+                <th className="py-4 text-left">Song</th>
+                <th className="py-4 text-left">Artist</th>
+                <th className="py-4 text-left">Year</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b hover:cursor-pointer hover:bg-slate-100">
+                <td className="py-3">
+                  The Sliding Mr. Bones (Next Stop, Pottersville)
+                </td>
+                <td>Malcolm Lockyer</td>
+                <td>1961</td>
+              </tr>
+              <tr className="border-b hover:cursor-pointer hover:bg-slate-100">
+                <td className="py-3">Witchy Woman</td>
+                <td>The Eagles</td>
+                <td>1972</td>
+              </tr>
+              <tr className="border-b hover:cursor-pointer hover:bg-slate-100">
+                <td className="py-3">Shining Star</td>
+                <td>Earth, Wind, and Fire</td>
+                <td>1975</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+
         <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
           <div className="sm:flex sm:flex-1 sm:items-center sm:justify-between">
             <div>
