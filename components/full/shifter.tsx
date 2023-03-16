5e99b3094e4edb34bc57515bfc84a3cf07ba32e7
Eldest Pasirula <aknutman@gmail.com>
2023-03-16 22:54:35 +0700

    fix

--- components/full/shifter.tsx
+++ components/full/shifter.tsx
@@ -2,7 +2,6 @@ import { ShifterModel } from "./model/shifter.model";
 import Sidebar from "../partial/sidebar";
 import BodyShifter from "../partial/body.shifter";
 import React from "react";
-import { Column } from "../partial/body.shifter/table.body.shifter/table.model";
 
 export default function Shifter(ShifterData: ShifterModel) {
   return (
