502bfcb401bcf6ac2f8486c58a5a5e6dacfe97b9
Eldest Pasirula <aknutman@gmail.com>
2023-03-16 22:53:21 +0700

    bug fix

--- components/full/model/shifter.model.ts
+++ components/full/model/shifter.model.ts
@@ -1,17 +1,5 @@
 import { Column } from "../../partial/body.shifter/table.body.shifter/table.model";
 
-/**
- * Every optional property, the default value will be considered as FALSY
- * Set the property name wise!
- */
-const OptionType = {
-  String: "string",
-  Number: "number",
-  Boolean: "boolean",
-  RichText: "richtext",
-  Options: "options",
-} as const;
-
 export interface ShifterModel {
   ShifterData: {
     TopBar?: {
