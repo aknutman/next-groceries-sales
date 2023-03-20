d6865956c04be1256947dcab33f39445ff986f4e
Eldest Pasirula <aknutman@gmail.com>
2023-03-20 20:43:06 +0700

    USER-ROLE model

---
+++ pages/s/system-master-user-role/index.tsx
@@ -0,0 +1,46 @@
+import Shifter from "../../../components/shifter/shifter";
+import { ShifterModel } from "../../../components/shifter/model/shifter.model";
+
+const data: ShifterModel = {
+  Body: {
+    Column: [
+      {
+        ColumnDefinition: "Id",
+        ColumnName: "Id",
+        Id: true,
+        Readonly: true,
+        OrderIndex: 1,
+      },
+      {
+        ColumnDefinition: "UniqueCode",
+        ColumnName: "Unique Code",
+        OrderIndex: 2,
+      },
+      {
+        ColumnDefinition: "RoleName",
+        ColumnName: "Role Name",
+        OrderIndex: 3,
+      },
+      {
+        ColumnDefinition: "RoleDescription",
+        ColumnName: "Role Description",
+        ValueType: "richtext",
+        OrderIndex: 4,
+      },
+      {
+        ColumnDefinition: "DocVersion",
+        ColumnName: "Doc Version",
+        Hidden: true,
+        OrderIndex: 5,
+        ValueType: "number",
+      },
+    ],
+    Row: {
+      SourceUrl: "USER-ROLE",
+    },
+  },
+};
+
+export default function SystemMasterUserRole() {
+  return <Shifter ShifterData={data} />;
+}
