fbd6c1ea20c35fa304d84eb6a45ee803a15c65b2
Eldest Pasirula <aknutman@gmail.com>
2023-03-22 10:37:36 +0700

    number input type

--- components/shifter/overlaybar.tsx
+++ components/shifter/overlaybar.tsx
@@ -144,6 +144,9 @@ function RenderInput({ detail, column }: InputBoxType) {
     case "richtext": {
       return <RichTextbox detail={detail} column={column} />;
     }
+    case "number": {
+      return <Numberbox detail={detail} column={column} />;
+    }
     case "boolean": {
       return <Checkbox detail={detail} column={column} />;
     }
@@ -242,6 +245,34 @@ function Checkbox({ detail, column }: InputBoxType) {
             {headerDesc !== "undefined" ? headerDesc : ""}
           </p>
         </div>
+      </div>
+    </>
+  );
+}
+
+function Numberbox({ detail, column }: InputBoxType) {
+  const headerName: string = String(
+    column.filter((col) => col.ColumnDefinition === detail.Id)[0].ColumnName
+  );
+
+  return (
+    <>
+      <div className="col-span-6">
+        <label
+          htmlFor={detail.Id}
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          {headerName}
+        </label>
+        <input
+          value={detail.value}
+          readOnly
+          type="number"
+          name={detail.Id}
+          id={detail.Id}
+          autoComplete={detail.Id}
+          className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
       </div>
     </>
   );
